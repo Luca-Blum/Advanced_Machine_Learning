11df5fb6ba5345e15618fe96edcc49c17f54b6c0
lblum <lblum@student.ethz.ch>
2020-10-31 14:39:24 +0100

    GridSearchCV done

--- task2/luca3.py
+++ task2/luca3.py
@@ -14,17 +14,7 @@ x_train = pd.read_csv("data/X_train.csv", index_col=0, header=0)
 y_train = pd.read_csv("data/y_train.csv", index_col=0, header=0)
 x_test = pd.read_csv("data/X_test.csv", index_col=0, header=0)
 
-
-max_C_sig = 0
-max_C_rbf = 0
-max_C_poly = 0
-max_C_final = 0
-max_dim = 0
-
-max_score = 0
-
 cv = StratifiedKFold(n_splits=5)
-
 
 estimators = [
     ('svc_sig', SVC(kernel='sigmoid', random_state=42)),
@@ -37,9 +27,8 @@ under = RandomUnderSampler(random_state=42)
 feature_sel = KernelPCA(kernel='rbf', random_state=42)
 model = StackingClassifier(estimators=estimators, final_estimator=SVC(random_state=42))
 
-
 param_grid = {
-    'feature__n_components': np.linspace(start=100, stop=600, num=6),
+    'feature__n_components': np.linspace(start=100, stop=600, num=2),
     'classification__svc_sig__C': [1],
     'classification__svc_rbf__C': [1],
     'classification__svc_poly__C': [1],
@@ -53,30 +42,14 @@ import pprint as pp
 
 pp.pprint(sorted(pipeline.get_params().keys()))
 
-search = GridSearchCV(pipeline, param_grid,scoring='balanced_accuracy', cv=cv, n_jobs=-1)
+search = GridSearchCV(pipeline, param_grid,scoring='balanced_accuracy', cv=cv, n_jobs=-1, verbose=10)
 search.fit(x_train, y_train.values.ravel())
 
 print("Best parameter (CV score=%0.3f):" % search.best_score_)
 print(search.best_params_)
+print(search.cv_results_)
 
-
-
-estimators = [
-    ('svc_sig', SVC(C=max_C_sig, kernel='sigmoid', random_state=42)),
-    ('svc_rbf', SVC(C=max_C_rbf, kernel='rbf', random_state=42)),
-    ('svc_poly', SVC(C=max_C_poly, kernel='poly', random_state=42))
-]
-
-over = SMOTE(random_state=42)
-under = RandomUnderSampler(random_state=42)
-feature_sel = KernelPCA(dim=max_dim, kernel='rbf', random_state=42)
-model = StackingClassifier(estimators=estimators, final_estimator=SVC(C=max_C_final, random_state=42))
-
-steps = [('feature sel', feature_sel), ('over', over), ('under', under), ('model', model)]
-pipeline = Pipeline(steps=steps)
-
-pipeline.fit(x_train, y_train.values.ravel())
-y_pred = pipeline.predict(x_test)
+y_pred = search.predict(x_test)
 
 df = pd.DataFrame(y_pred)
 df.to_csv('data/y_pred.csv', header=['y'], index_label='id')
